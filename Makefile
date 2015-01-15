73949f6d7d52b3ea87a6d2fe2f32de927877de22
Michael Redman <michael.redman.4@gmail.com>
2015-01-15 08:35:56 +0000

    added program for faster lookups of path of each hash in a set

--- Makefile
+++ Makefile
@@ -3,18 +3,18 @@ exec_prefix?=$(prefix)
 
 QUERY_TYPE?=JOIN
 
-PROGS=paths hashes update
+PROGS=list hashes update
 SCRIPTS=diff clean
 DOCS=COPYRIGHT LICENSE README
 
 CFLAGS=-Wall -g -fstack-protector -O2 -I /usr/include/postgresql -I $(prefix)/include -D$(QUERY_TYPE) 
 LDFLAGS=-L /usr/lib/postgresql -lpq -L $(exec_prefix)/lib -L . -L $(exec_prefix)/lib/verity
 
-all: $(LIBS) $(LIBEXECS) $(PROGS)
+all: $(LIBS) $(LIBEXECS) $(PROGS) paths
 
 .PHONY: clean
 clean:
-	rm -f $(LIBS) $(LIBEXECS) $(PROGS)
+	rm -f $(LIBS) $(LIBEXECS) $(PROGS) paths
 
 hashes: hashes.c
 	cc $(CFLAGS) $(LDFLAGS) -lfgetsnull -o $@ $<
@@ -22,15 +22,20 @@ hashes: hashes.c
 update: update.c sha256_of_file.c
 	cc $(HACKS) $(CFLAGS) $(LDFLAGS) -lfgetsnull -lhexbytes -lcrypto -o $@  $^
 
+paths: paths.c
+	cc -Wall -g -fstack-protector -O2 -o $@ $<
+
 install:
 	$(foreach prog, $(PROGS), install -D -m 0755 $(prog) $(exec_prefix)/bin/verity_$(prog); )
 	$(foreach prog, $(SCRIPTS), install -D -m 0755 $(prog) $(exec_prefix)/bin/verity_$(prog); )
 	$(foreach prog, $(DOCS), install -D -m 0644 $(prog) $(prefix)/share/doc/verity/$(prog); )
+	install -D -m 0755 paths $(exec_prefix)/lib/verity/paths
 	install -D -m 0644 schema.psql $(prefix)/share/verity/schema.psql
 
 uninstall:
 	$(foreach prog, $(PROGS) $(SCRIPTS), rm $(exec_prefix)/bin/verity_$(prog); )
 	rm -rf $(prefix)/share/doc/verity
 	rm -rf $(prefix)/share/verity
+	rm -rf $(exec_prefix)/lib/verity
 	
 #IN GOD WE TRVST.
--- paths.c
+++ paths.c
@@ -1,93 +1,74 @@
-#define USE "verity_list [-c] [-t as-of-time_t] 'db connection string'"
+#define USE "paths infile < hashlist\n"
 
-#define _GNU_SOURCE
-
-#include <libpq-fe.h>
 #include <linux/limits.h>
+#include <math.h>
+#include <openssl/sha.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/stat.h>
 #include <time.h>
-#include <unistd.h>
 
-#include "err.h"
+#include <fgetsnull.h>
 
-#define SQL_DECLARE	"declare paths_cursor cursor for select paths.path "
+#include "at.h"
 
-#ifndef CORRELATED_SUBQUERY
+#define HASH_LEN (2*SHA256_DIGEST_LENGTH)
 
-#define JOIN0	", mode, content "
-#define JOIN1	"from paths "\
-		"join (select path, max(xtime) as max_xtime from paths where xtime<=%ld group by path) as max_xtimes on max_xtimes.path=paths.path and max_xtimes.max_xtime=paths.xtime "
-#define JOIN2	"join inodes on inodes.device=paths.device and inodes.inode=paths.inode and inodes.ctime=paths.ctime "
-#define JOIN3	"where paths.device is not null"
+struct lookup_s{
+	char hash[HASH_LEN];
+	unsigned long long offset; };
 
-#else
-
-#define SQ0	"(select mode from inodes where inodes.device=paths.device and inodes.inode=paths.inode and inodes.ctime=paths.ctime) as mode, "\
-		"(select content from inodes where inodes.device=paths.device and inodes.inode=paths.inode and inodes.ctime=paths.ctime) as content "
-#define SQ1	"from paths where "\
-		"device is not null "\
-		"and not exists (select * from paths as alias where alias.path=paths.path and alias.xtime>paths.xtime and alias.xtime<=%ld)"
-
-#endif
+int compar (void const *a, void const *b)
+{	return memcmp(
+		((struct lookup_s *)a)->hash,
+		((struct lookup_s *)b)->hash,
+		HASH_LEN); }
 
 int main(int argc, char ** argv){
-	PGconn *db;
-	PGresult *result;
-	char *sql, *sql_f, flag=0;
-	time_t t=time(NULL);
+	struct lookup_s * base=NULL, *p;
+	unsigned int n_data=0, n_alloc=0, alloc_try;
+	unsigned long long offset=0;
 	int c;
-	while ((c=getopt(argc,argv,"ct:"))!=-1){ switch(c){
-		case 'c': flag=1; break;
-		case 't': t=strtoll(optarg,NULL,10); break;
-		default: fputs(USE,stderr); exit(EXIT_FAILURE); }}
-	if (!(argc-optind)) { fputs(USE,stderr); exit(EXIT_FAILURE); }
-	db=PQconnectdb(argv[optind]);
-	if(PQstatus(db)!=CONNECTION_OK){
-		fputs(PQerrorMessage(db),stderr);
-		exit(EXIT_FAILURE); }
-	result=PQexec(db,"begin");
-	SQLCHECK(db,result,PGRES_COMMAND_OK,err0);
-	#ifndef CORRELATED_SUBQUERY
-	if	(flag)
-		sql_f=SQL_DECLARE JOIN0 JOIN1 JOIN2 JOIN3;
-		else sql_f=SQL_DECLARE JOIN1 JOIN3;
-	#else
-	if	(flag)
-		sql_f=SQL_DECLARE SQ0 SQ1;
-		else sql_f=SQL_DECLARE SQ1;
-	#endif
-	if	(asprintf(&sql,sql_f,t)==-1)
-		{ fputs("asprintf failed",stderr); goto err0; }
-	PQclear(result);
-	result=PQexec(db,sql);
-	free(sql);
-	SQLCHECK(db,result,PGRES_COMMAND_OK,err0);
-	PQclear(result);
-	while(1){
-		result=PQexec(db,"fetch paths_cursor");
-		SQLCHECK(db,result,PGRES_TUPLES_OK,err0);
-		if(!PQntuples(result)) break;
-		if	(flag)
-			{	if	(!S_ISREG(strtol(PQgetvalue(result,0,1),NULL,10)))
-					{	PQclear(result);
-						continue; }
-				fputs(PQgetvalue(result,0,2),stdout); }
-		printf("%s%c",PQgetvalue(result,0,0),'\0');
-		PQclear(result); }
-	PQclear(result);
-	result=PQexec(db,"close paths_cursor");
-	SQLCHECK(db,result,PGRES_COMMAND_OK,err0);
-	PQclear(result);
-	result=PQexec(db,"end");
-	SQLCHECK(db,result,PGRES_COMMAND_OK,err0);
-	PQclear(result);
-	PQfinish(db);
-	exit(EXIT_SUCCESS);
-	err0:	PQclear(result);
-		PQfinish(db);
+	FILE * stream;
+	char hash[HASH_LEN+2];
+	if	(!(stream=fopen(argv[1],"rb")))
+		{ perror(argv[1]); return 1; }
+	while	(!feof(stream))
+		{	if	(n_data==n_alloc)
+				{	alloc_try=1+3*n_alloc;
+					p=realloc(base,alloc_try*sizeof(struct lookup_s));
+					if (!p) { perror("realloc failed " AT); goto l_0_0; }
+					base=p;
+					n_alloc=alloc_try; }
+			if (fread(&base[n_data],HASH_LEN,1,stream)!=1) break;
+			base[n_data++].offset=offset;
+			offset+=HASH_LEN;
+			do	{	c=fgetc(stream);
+					if (c==EOF) { fprintf(stderr,"path not read for hash %s " AT, hash); goto l_0_0; }
+					offset++;
+					if (!c) break; }
+				while(!feof(stream)); }
+	if (ferror(stream)) { perror(argv[1]); AT_ERR; goto l_0_0; }
+	while	(!feof(stdin))
+		{	if	(!fgets(hash,HASH_LEN+2,stdin))
+				break; 
+			hash[HASH_LEN]='\0';
+			if	(!(p=bsearch(hash,base,n_data,sizeof(struct lookup_s),compar)))
+				{	fprintf(stderr,"path not found for %s\n",hash);
+					continue; }
+			if	(fseek(stream,p->offset+HASH_LEN,SEEK_SET))
+				{ perror(argv[1]); AT_ERR; goto l_0_0; }
+			fputs(hash,stdout);
+			do	{	c=fgetc(stream);
+					if (c==EOF) { fprintf(stderr,"short read of path for hash %s " AT, hash); goto l_0_0; }
+					fputc(c,stdout);
+					if (!c) break; }
+				while(!feof(stdin)); }
+	if (base) free(base);
+	if (fclose(stream)) { perror(argv[1]); return 1; }
+	return 0;
+	l_0_0:	if (base) free(base);
+		if (fclose(stream)) perror(argv[1]);
 		exit(EXIT_FAILURE); }
 
-/*IN GOD WE TRVST.*/
+//IN GOD WE TRVST.
